242d3e8ba926155235a09f6564587f737bd529ff
Syed Abrar <syedabrar003@gmail.com>
2020-10-31 07:49:53 +0100

    git init; program skeleton

---
+++ src/Government.java
@@ -0,0 +1,17 @@
+public class Government {
+    Government (String configurationFile) {
+
+    }
+
+    boolean mobileContact (String initiator, String contactInfo) {
+        return true;
+    }
+
+    void recordTestResult (String testHash, int date, boolean result) {
+
+    }
+
+    int findGatherings (int date, int minSize, int minTime, float density) {
+        return 0;
+    }
+}
---
+++ src/MobileDevice.java
@@ -0,0 +1,17 @@
+public class MobileDevice {
+    MobileDevice (String configurationFile, Government contactTracer) {
+
+    }
+
+    void recordContact (String individual, int date, int duration) {
+
+    }
+
+    void positiveTest (String testHash) {
+
+    }
+
+    boolean synchronizeData () {
+        return true;
+    }
+}
