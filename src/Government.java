03cdd6b8f1ecdf6a8d54216b43b451f599c04171
Syed Abrar <syedabrar003@gmail.com>
2020-11-17 14:48:49 +0100

    add tentative/ planned method calls

--- src/Government.java
+++ src/Government.java
@@ -1,17 +1,35 @@
-public class Government {
-    Government (String configurationFile) {
+import java.io.IOException;
 
+public class Government {
+    Configuration config;
+    DB db;
+
+    Government (String configurationFile) {
+        config.readFile(configurationFile);
+        db = new DB(config.getValue("addr"), config.getValue("username")));
     }
 
     boolean mobileContact (String initiator, String contactInfo) {
+        // TODO for each contact
+        Contact.fromXML(contactInfo);
+        DB.storeContact(initiator, contact);
+        return true;
+    }
+
+    // Separate method for communicating a test result, in this demo, called
+    // directly by MobileDevice.synchronizeData
+    boolean mobileTest (String initiator, String testInfo) {
+        // Government only knows the test hash so far, update the record to store the correct device id as well
+        DB.setTestDeviceHash(initiator, testHash);
         return true;
     }
 
     void recordTestResult (String testHash, int date, boolean result) {
-
+        DB.storeTestResult(testHash, date, result);
     }
 
     int findGatherings (int date, int minSize, int minTime, float density) {
+        DB.getContactsOnDate(date);
         return 0;
     }
 }
--- src/MobileDevice.java
+++ src/MobileDevice.java
@@ -1,6 +1,13 @@
-public class MobileDevice {
-    MobileDevice (String configurationFile, Government contactTracer) {
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+public class MobileDevice {
+    Configuration config;
+    List<Contact> recentContacts = new ArrayList<>();
+
+    MobileDevice (String configurationFile, Government contactTracer) {
+        config.readFile(configurationFile);
     }
 
     void recordContact (String individual, int date, int duration) {
